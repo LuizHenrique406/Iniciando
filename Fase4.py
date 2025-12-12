40b3bff59548243cbdf3eb19ec2dc137c857e5f4
Luiz Henrique <luiz.henriqueteixeira2008@gmail.com>
2025-12-11 21:47:55 -0300

    Update Fase4.py
    Updated harvesting logic to include an else condition for movement.

--- Fase4.py
+++ Fase4.py
@@ -1,6 +1,8 @@
-# colheita automática,  mais eficiente mais zona de colheitas
+# mesma situação que a fase3, porém, com o else.
 while True:
 	if can_harvest():
 		harvest()
+
+	#se ele não pode colher, move para o norte, voltando para o ínicio do código e repetindo tudo novamente.
 	else:
 		move(North)
